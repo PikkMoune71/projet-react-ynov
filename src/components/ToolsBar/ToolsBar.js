4b74589a364309dc64139e38935e7ca012cebc70
Antoine Picard <antoinepicard385@gmail.com>
2022-11-02 11:23:16 +0100

    add the Response button with context

--- src/components/ToolsBar/ToolsBar.js
+++ src/components/ToolsBar/ToolsBar.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
-import { ThemeContext, themes } from '../../context/ThemeContext';
+import { ThemeContext, themes, responses } from '../../context/ThemeContext';
 import Button from '../Button/Button';
 import './ToolBar.scss';
 import * as htmlToImage from 'html-to-image';
 import download from 'downloadjs'
-import { faMoon, faSave, faSun } from '@fortawesome/free-solid-svg-icons'
+import { faMoon, faSave, faSun, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
 
 const ToolsBar = () => {
     const [darkMode, setDarkMode] = useState(true);
+    const [responseMode, setResponseMode] = useState(true);
 
     function savePng() {
         htmlToImage.toPng(document.getElementById('Resizable'))
@@ -19,20 +21,36 @@ const ToolsBar = () => {
     return (
         <div className='containerToolsbar'>
             <ThemeContext.Consumer>
-            {({ changeTheme }) => (
-                <Button
-                class="btn-toolsbar"
-                name="Card"
-                icon={darkMode ? faMoon : faSun}
-                click={() => {
-                    setDarkMode(!darkMode);
-                    changeTheme(darkMode ? themes.dark : themes.light);
-                }}
-                >
-                </Button>
-            )}
+                {({ changeTheme }) => (
+                    <Button
+                    class="btn-toolsbar"
+                    name="Card"
+                    icon={darkMode ? faMoon : faSun}
+                    click={() => {
+                        setDarkMode(!darkMode);
+                        changeTheme(darkMode ? themes.dark : themes.light);
+                    }}
+                    >
+                    </Button>
+                )}
             </ThemeContext.Consumer>
-            <Button class="btn-toolsbar" click={savePng} name="Save" icon={faSave}></Button>
+            <ThemeContext.Consumer>
+                {({ changeResponse }) => (
+                    <Button
+                    class="btn-toolsbar"
+                    name="Response"
+                    icon={responseMode ? faHeart : faHeartRegular}
+                    click={() => {
+                        setResponseMode(!responseMode);
+                        changeResponse(responseMode ? responses.hidden : responses.show);
+                    }}
+                    >
+                    </Button>
+                )}
+            </ThemeContext.Consumer>
+            <div className='content-save'>
+                <Button class="btn-toolsbar save" click={savePng} name="Save" icon={faSave}></Button>
+            </div>
         </div>
     )
 }
--- src/context/ThemeContextWrapper.js
+++ src/context/ThemeContextWrapper.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { ThemeContext, themes } from './ThemeContext';
+import { ThemeContext, themes, responses} from './ThemeContext';
 
 export default function ThemeContextWrapper(props) {
   const [theme, setTheme] = useState(themes.light);
+  const [response, setResponse] = useState(responses.show)
 
   function changeTheme(theme) {
     setTheme(theme);
+  }
+
+  function changeResponse(response){
+    setResponse(response);
   }
 
   useEffect(() => {
@@ -20,8 +25,20 @@ export default function ThemeContextWrapper(props) {
     }
   }, [theme]);
 
+  useEffect(() => {
+    switch (response) {
+      case responses.hidden:
+        document.getElementById('interactivities').style.display = "none";
+        break;
+      case responses.show:
+      default:
+        document.getElementById('interactivities').style.display = "flex";
+        break;
+    }
+  }, [response]);
+
   return (
-    <ThemeContext.Provider value={{ theme: theme, changeTheme: changeTheme }}>
+    <ThemeContext.Provider value={{ theme: theme, response: response, changeTheme: changeTheme, changeResponse: changeResponse }}>
       {props.children}
     </ThemeContext.Provider>
   );

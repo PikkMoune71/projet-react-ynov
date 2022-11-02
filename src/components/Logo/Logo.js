3cafef0f3dcc15c6af49ce775729844ffc42f538
Antoine Picard <antoinepicard385@gmail.com>
2022-11-02 15:25:29 +0100

    add logo + title

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Logo.scss'
+import Twimage from '../../assets/img/Twimage-logo.png'
+
+const Logo = () => {
+  return (
+    <img src={Twimage} alt='' className='logo'/>
+  )
+}
+
+export default Logo;
